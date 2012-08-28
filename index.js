c723b77d02ecbba64d1ea19686df4652cb3213d8
tokuhirom <tokuhirom@gmail.com>
2012-08-28 09:19:18 +0900

    import ppport.h

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 var P = require('./build/Release/perl-simple.node');
 P.InitPerl();
+P.Perl.prototype.use = function (name) {
+    if (!name.match(/^[A-Za-z0-9_:]+$/)) {
+        throw new Exception("This is not a valid class name : " + name);
+    }
+    return this.evaluate('use ' + name);
+};
 module.exports.Perl = P.Perl;
