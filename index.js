fe58100bda7a4cda029e37bd7ad23da0fe81558c
Valerii Iatsko <dwr@codingbox.io>
2016-09-02 13:35:02 +0200

    Fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var P = require('./build/Release/perl.node');
-P.InitPerl();
 P.Perl.prototype.use = function (name) {
     if (!name.match(/^[A-Za-z0-9_:]+$/)) {
         throw new Exception("This is not a valid class name : " + name);
