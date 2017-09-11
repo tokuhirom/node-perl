6b48de8e81ed993360b770bab8f28773b1de4385
bstriker <strikeforcezero@gmail.com>
2017-09-11 02:00:45 -0400

    Use Error class instead of the unreferenced class Exception

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var P = require('./build/Release/perl.node');
 P.InitPerl();
 P.Perl.prototype.use = function (name) {
     if (!name.match(/^[A-Za-z0-9_:]+$/)) {
-        throw new Exception("This is not a valid class name : " + name);
+        throw new Error("This is not a valid class name : " + name);
     }
     return this.evaluate('use ' + name);
 };
